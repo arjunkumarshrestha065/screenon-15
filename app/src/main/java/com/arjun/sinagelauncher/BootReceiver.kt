ec71a44424e62b592db8bdec2db8500f7f25edc8
arjunkumarshrestha065 <arjunkumarshrestha065@gmail.com>
2026-09-06 17:31:40 +0545

    Create BootReceiver.kt

---
+++ app/src/main/java/com/arjun/sinagelauncher/BootReceiver.kt
@@ -0,0 +1,23 @@
+package com.arjun.sinagelauncher
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+        if (action == Intent.ACTION_BOOT_COMPLETED ||
+            action == Intent.ACTION_LOCKED_BOOT_COMPLETED ||
+            action == "android.intent.action.QUICKBOOT_POWERON" ||
+            action == Intent.ACTION_MY_PACKAGE_REPLACED) {
+
+            val launchIntent = Intent(context, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            }
+            context.startActivity(launchIntent)
+        }
+    }
+}
